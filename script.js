1490fda0103c0da26ad92416832c78445f9c3763
Nicole Nigro <nnigro@bowdoin.edu>
2022-02-01 15:34:36 -0500

    working on folders

---
+++ script.js
@@ -0,0 +1,9 @@
+function researchFolder(){
+    var r = document.getElementById("research");
+    r.innerHTML = "research folder open";
+}
+
+function projectsFolder(){
+    var p = document.getElementById("projects");
+    p.innerHTML = "projects folder open";
+}
