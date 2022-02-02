89c2d7300b3aa52ce70bb583f745799b4221e8ff
Nicole Nigro <nnigro@bowdoin.edu>
2022-02-02 16:55:04 -0500

    changesssss

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function researchFolder(){
     var r = document.getElementById("research");
-    r.innerHTML = "research folder open";
 }
 
 function projectsFolder(){
     var p = document.getElementById("projects");
-    p.innerHTML = "projects folder open";
 }
